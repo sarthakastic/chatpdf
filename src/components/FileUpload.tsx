1e418fd14aa896e620f91a140681b589a805dcfd
guptasarthak2605 <sarthak.gupta@canopi.in>
2023-09-20 15:52:28 +0530

    add : aws s3

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { uploadtoS3 } from "@/lib/s3";
 import { Inbox } from "lucide-react";
 import React from "react";
 import { useDropzone } from "react-dropzone";
@@ -9,8 +10,19 @@ const FileUpload = (props: Props) => {
   const { getRootProps, getInputProps } = useDropzone({
     accept: { "application/pdf": [".pdf"] },
     maxFiles: 1,
-    onDrop: (acceptedFiles) => {
+    onDrop: async (acceptedFiles) => {
       console.log(acceptedFiles);
+      const file = acceptedFiles[0];
+      if (file.size > 10 * 1024 * 1024) {
+        alert("Please Upload a file less than 10mb");
+        return;
+      }
+      try {
+        const data = await uploadtoS3(file);
+        console.log("data", data);
+      } catch (error) {
+        console.log(error);
+      }
     },
   });
 
